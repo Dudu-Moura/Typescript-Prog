04cff6be147cafaae09fd2b2bb9cf5f891820fa5
Dudu-Moura <eduardo.moura2007@hotmail.com>
2026-05-03 21:48:14 -0300

    chore: adicionando variaveis de ambiente

--- 03-http_express/server/index.ts
+++ 03-http_express/server/index.ts
@@ -1,10 +1,11 @@
+import 'dotenv/config'
+import { env } from "./config/env";
 import express  from "express"
 import { Request, Response, NextFunction } from "express";
 import livroRouter  from "../server/routes/livro.routes"
 import emprestimoRouter from "../server/routes/emprestimo.routes"
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 
@@ -17,6 +18,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ erro: err.message });
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`)
+app.listen(Number(env.PORT), () => {
+    console.log(`Servidor rodando em http://localhost:${env.PORT}`)
 })
