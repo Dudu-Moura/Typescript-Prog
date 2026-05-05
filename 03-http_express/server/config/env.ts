8497b096fd7e5ab07e5ad10a59bc9035f1512cd0
Dudu-Moura <eduardo.moura2007@hotmail.com>
2026-05-04 22:43:10 -0300

    feat: adicionando ex de variaveis de ambiente

--- 03-http_express/server/config/env.ts
+++ 03-http_express/server/config/env.ts
@@ -2,6 +2,7 @@ function carregarEnv() {
     const variaveis = {
         PORT: process.env.PORT ?? '3000',
         NODE_ENV: process.env.NODE_ENV ?? 'development',
+        DATABASE_URL: process.env.DATABASE_URL
     };
 
     const faltando = Object.entries(variaveis)
