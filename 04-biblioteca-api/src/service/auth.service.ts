352b6f0a4a3e4fe2e2c171985ec59e7a5a1293f3
Dudu-Moura <eduardo.moura2007@hotmail.com>
2026-05-08 17:08:43 -0300

    feat: começando a criar o service

--- 04-biblioteca-api/src/service/auth.service.ts
+++ 04-biblioteca-api/src/service/auth.service.ts
@@ -1,8 +1,22 @@
+import bcrypt from "bcryptjs";
 import { CreateLogin } from "../dtos/auth.dto";
 import { ConflictError } from "../errors/AppError";
 import { Usuario } from "../generated/prisma/client";
 import { UsuarioRepository } from "../repository/usuario.repository";
+import * as jwt from 'jsonwebtoken';
 
 export class AuthService{
+    constructor(private UsuarioRepo: UsuarioRepository){};
 
+    async criarLogin(dados: CreateLogin): Promise<{ token: string }>{
+
+        const userEmail = await this.UsuarioRepo.findByEmail(dados.email);
+
+        if(userEmail) throw new ConflictError('Usuário já existente');
+
+        const senha = await bcrypt.hash(dados.senha, 10);
+
+        const usuario = {...dados, senha};
+        await this.UsuarioRepo.create(usuario);
+    }
 }
